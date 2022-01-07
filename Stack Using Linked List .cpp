43fa1e2c5abd4922f11e181b28064f32d47a5192
‪Karim Ehab‬‏ <87093504+KarimEhab1@users.noreply.github.com>
2022-01-07 10:52:32 +0200

    Update Stack Using Linked List .cpp

--- Stack Using Linked List .cpp
+++ Stack Using Linked List .cpp
@@ -2,90 +2,81 @@
 using namespace std;
 #define vi vector<int>
 #define ll long long
-template<class t>
-class Stack{
-    struct node{
-        t item ;
-        node *next;
-    };
-    node *top,*cur;
-
-public:
-     Stack (): top(NULL),cur(NULL){}
-    void push(t newitem){
-        node*newitemptr=new node;
-        if (newitemptr == NULL)
-			cout << "Stack push cannot allocate memory";
-        else{
-        newitemptr->item=newitem;
-        newitemptr->next=top;
-        top=newitemptr;
-        }
-    }
-    bool isEmpety(){
-        return top=NULL;
-    }
-    void pop(){
-        if(isEmpety())
-        {
-            cout << "Stack empty on pop";
-        }
-        else{
-        node*temp=top;
-        top=top->next;
-        temp->next=NULL;
-        delete temp;
-            }
-        }
-    void pop(t&stackTop){
-        if(isEmpety())
-        {
-            cout << "Stack empty on pop";
-        }
-        else{
-        stackTop=top->item;
-        node temp=top;
-        top=top->next;
-        temp->next=NULL;
-        delete temp;
-            }
-        }
-    void getTop(t&stackTop){
-        if(isEmpety())
-        {
-            cout << "Stack empty on pop";
-        }
-        else{
-            stackTop=top->item;
-        }
-    }
-    void display(){
-        cur=top;
-        cout << "\nItems in the stack : ";
-		cout << "[ ";
-		while(cur!=NULL){
-            cout << cur->item << " ";
-            cur=cur->next;
-		}
-        cout << " ]\n";
-    }
-
-
-};
 void fast(){
 	ios_base::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
 }
-int main()
-{
-	fast();
-	Stack<int>s;
-	s.push(100);
-	s.push(200);
-	s.push(300);
-	
-	s.display();
-	return 0;
-}
+template<class t>
+class linkedStack{
+    struct node{
+    t iteam;
+    node *next;
+    };
+    node *top;
+public:
+    linkedStack():top(NULL){}
+    void push(t newIteam){
+        node *newNode= new node;
+        newNode->iteam= newIteam;
 
+        newNode->next= top;
+        top= newNode;
+    }
+    bool isEmpty(){
+        return top==NULL;
+    }
+    void pop(){
+        if(isEmpty()){
+            cout << " stack is empty ";
+        }
+        else
+        {
+        node *temp=top;
+        top= top->next;
+        temp=temp->next= NULL;
+        delete temp;
+        }
+    }
+    void pop(t &stackTop){
+        if(isEmpty()){
+            cout << " stack is empty ";
+        }
+        else
+        {
+        stackTop= top->iteam;
+        node *temp=top;
+        top= top->next;
+        temp=temp->next= NULL;
+        delete temp;
+        }
+    }
+     void getTop(t &stackTop){
+        if(isEmpty()){
+            cout << " stack is empty ";
+        }
+        else
+        {
+            stackTop= top->iteam;
+        }
+    }
+    void display(){
+        node *cur=top;
+        while(cur!=NULL){
+            cout << cur->iteam << "\n";
+            cur=cur->next;
+        }
+    }
+};
+
+int main(){
+    fast();
+    linkedStack <int> ls;
+    ls.push(10);
+    ls.push(20);
+    ls.push(30);
+    ls.push(40);
+    ls.display();
+
+return 0;
+}
